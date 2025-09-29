c0bb0e34a72bbb66bf26d8b2bb7f3bc03ddc12a5
Vikas Kumar <akujvika@amazon.com>
2025-09-29 18:15:55 +0530

    Add payroll GitHub Actions workflow

--- src/main/java/com/hrms/model/service/PayrollJob.java
+++
@@ -1,8 +0,0 @@
-package com.hrms.model.service;
-
-public class PayrollJob {
-    public static void main(String[] args) {
-        PayrollService payrollService = new PayrollService();
-        payrollService.generatePayrollForAll();
-    }
-}
